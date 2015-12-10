f2e1929420e3fd46bfe92a6d4d00677cfe131520
Tugberk Ugurlu <tugberk@outlook.com>
2015-12-10 17:12:52 +0000

    broken sql

--- db/Tables/dbo.Visits.sql
+++ db/Tables/dbo.Visits.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[Visits]
 (
 [Id] [int] NOT NULL IDENTITY(1, 1),
 [IpAddress] [nvarchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[CreatedOn] [datetimeoffset] NOT NULL
+[CreatedOn] [datetimeoffset] NOT NULL,
+[LastUpdatedOn] [datetimeoffset] NOT NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Visits] ADD CONSTRAINT [PK_Visits] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
